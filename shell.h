53d2462cb91afcb584d0504da1155562373160a3
salmanben-CS <salmanbenomar250@gmail.com>
2023-07-24 00:16:26 +0100

    message

--- shell.h
+++ shell.h
@@ -12,6 +12,15 @@
 #include <time.h>
 #include <stdbool.h>
 
+void betty_test(void);
+unsigned int check_delim(char c, const char *str);
+char *_strtok(char *str, const char *delim);
+char *_getline();
+void hashtag_h(char *buff);
+int _strlen(char *str);
+void print_environment(void);
+int check_c(int readBytes);
+void _execute(void);
 /* environment variables */
 extern char **environ;
 extern __sighandler_t signal(int __sig, __sighandler_t __handler);
