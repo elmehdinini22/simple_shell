1b10d7c894e3c71eb8624fd7a3740eed991ebdc1
salmanben-CS <salmanbenomar250@gmail.com>
2023-07-26 05:45:11 -0700

    fix error in execute_c.c file

--- execute_c.c
+++ execute_c.c
@@ -1,95 +1,86 @@
 #include "shell.h"
+
 /**
- * read_file - Read Command
- * @filename:Filename
- * @argv:Program Name
- * Return: -1 or  0
+ * check_c - Check if the command is valid and executable.
+ * @readBytes: The number of bytes read from the input.
+ *
+ * Return:
+ *  0 - If the command is empty or an error occurred while reading.
+ *  1 - If the command is empty (only a newline).
+ *  2 - If the command is valid and executable.
  */
-
-void read_file(char *filename, char **argv)
+int check_c(int readBytes)
 {
-	FILE *fp;
-	char *line = NULL;
-	size_t len = 0;
-	int counter = 0;
+    char command[10];
 
-	fp = fopen(filename, "r");
-	if (fp == NULL)
-	{
-		exit(EXIT_FAILURE);
-	}
-	while ((getline(&line, &len, fp)) != -1)
-	{
-		counter++;
-		treat_file(line, counter, fp, argv);
+    if (readBytes < 1)
+    {
+        if (readBytes == 0)
+            perror("Error: Empty command\n");
+        else
+            perror("Error in reading command\n");
+        return (0);
+    }
+    else if (readBytes == 1 && command[0] == '\n')
+    {
+        return (1);
+    }
 
-	}
-	if (line)
-		free(line);
-	fclose(fp);
-	exit(0);
+    if (access(command, F_OK | X_OK) == -1)
+    {
+        perror("Error: executable cannot be found\n");
+        return (0);
+    }
+
+    return (2);
 }
+
 /**
- * treat_file - PARSE Check Command Fork
- * @line: Line From A File
- * @counter:Error Counter
- * @fp:File Descriptor
- * @argv:Program Name
- * Return : Excute A line void
+ * _execute - Execute the command line interpreter.
+ *
+ * return: void
  */
-void treat_file(char *line, int counter, FILE *fp, char **argv)
+void _execute(void)
 {
-	char **cmd;
-	int st = 0;
+    ssize_t bytesRead;
+    pid_t id;
+    char command[10];
+    int status;
+    char *argv[2];
+    int valid_c;
+    char *envp[] = {NULL};
+    char prompt[17] = "Enter a command\n";
 
-	cmd = parse_cmd(line);
+    while (1)
+    {
+        write(1, prompt, sizeof(prompt) - 1);
+        bytesRead = read(0, command, sizeof(command) - 1);
 
-		if (_strncmp(cmd[0], "exit", 4) == 0)
-		{
-			exit_bul_for_file(cmd, line, fp);
-		}
-		else if (check_builtin(cmd) == 0)
-		{
-			st = handle_builtin(cmd, st);
-			free(cmd);
-		}
-		else
-		{
-			st = check_cmd(cmd, line, counter, argv);
-			free(cmd);
-		}
-}
-/**
- * exit_bul_for_file - Exit Shell
- * @line: Line From A File
- * @cmd: Parsed Command
- * @fd:File Descriptor
- * Return : Case Of Exit in A File Line
- */
-void exit_bul_for_file(char **cmd, char *line, FILE *fd)
-{
-	int statue, i = 0;
+        valid_c = check_c(bytesRead);
+        if (valid_c == 0)
+            continue;
+        else if (valid_c == 1)
+            break;
 
-	if (cmd[1] == NULL)
-	{
-		free(line);
-		free(cmd);
-		fclose(fd);
-		exit(errno);
-	}
-	while (cmd[1][i])
-	{
-		if (_isalpha(cmd[1][i++]) < 0)
-		{
-			perror("illegal number");
-		}
-	}
-	statue = _atoi(cmd[1]);
-	free(line);
-	free(cmd);
-	fclose(fd);
-	exit(statue);
+        id = fork();
+        if (id == -1)
+        {
+            perror("Error in creating process\n");
+            continue;
+        }
 
-
-
+        if (id == 0)
+        {
+            command[bytesRead - 1] = '\0';
+            argv[0] = command;
+            argv[1] = NULL;
+            execve(command, argv, envp);
+            perror("Error in command execution ");
+            _exit(EXIT_FAILURE);
+        }
+        else
+        {
+            wait(&status);
+        }
+    }
 }
