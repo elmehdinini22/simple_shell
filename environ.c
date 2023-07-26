f093454a7aafaa8e74183e4bd271b63048721d31
elmehdinini22 <elmehdinini1@gmail.com>
2023-07-26 15:56:11 -0700

    messag

--- environ.c
+++ environ.c
@@ -48,7 +48,7 @@ int _mysetenv(info_t *info)
 		_puts("Incorrect number of arguements\n");
 		return (1);
 	}
-	if (_setenv(info, info->argv[1], info->argv[2]))
+	if (_getenv(info, info->argv[1], info->argv[2]))
 		return (0);
 	return (1);
 }
